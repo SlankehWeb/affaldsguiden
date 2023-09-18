df61e6576d6526f795d3b1e32e3f8ef3f36f7382
SlankehWeb <101549379+SlankehWeb@users.noreply.github.com>
2023-09-18 14:41:24 +0200

    frontpage layout and base styling

--- src/components/pages/frontpage/frontpage.jsx
+++ src/components/pages/frontpage/frontpage.jsx
@@ -1,10 +1,12 @@
 import Carousel from "./parts/carousel/carousel";
+import FrontPageArticals from "./parts/frontpagearticals/frontpagearticals";
 
 
 const Frontpage = () => {
   return (
     <>
     <Carousel/>
+    <FrontPageArticals/>
     </>
   );
 };
