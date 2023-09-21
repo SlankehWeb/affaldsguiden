f0782365db952925fa4ebaa068b97e42a1508f42
SlankehWeb <101549379+SlankehWeb@users.noreply.github.com>
2023-09-21 22:26:51 +0200

    sorting details fixed

--- src/components/pages/sortingpage/sortingdetails/sortingdetails.jsx
+++ src/components/pages/sortingpage/sortingdetails/sortingdetails.jsx
@@ -1,18 +1,16 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
+import "./sortingdetails.scss";
 
 const SortingDetails = () => {
   const [data, setData] = useState([]);
 
-  // Get the 'id' parameter from the URL using React Router
   const { id } = useParams();
 
-  // Fetch event data from the server when the 'id' parameter changes
   useEffect(() => {
     const url = `http://localhost:4000/section/${id}`;
 
-    // Function to fetch data asynchronously
     const getData = async () => {
       try {
         const result = await axios.get(url);
@@ -23,30 +21,36 @@ const SortingDetails = () => {
       }
     };
 
-    getData(); // Call the getData function when the 'id' parameter changes
-  }, [id]); // This effect will run whenever 'id' changes
+    getData();
+  }, [id]);
 
   return (
     <>
       {data && (
         <>
-          <figure className="readCard" key={data.id}>
-            <h2>{data.title}</h2>
-        <img
-                      src={`http://localhost:4000/Assets/Images/Guide/Categories/${data.filename}`}
-                      alt={data.title}
-                    />
+          <figure className="sortingcard" key={data.id}>
+            <div
+              className="topcard"
+              style={{ backgroundColor: `#${data.color}` }}
+            >
+              <h2>{data.title}</h2>
+              <img
+                src={`http://localhost:4000/Assets/Images/Guide/Categories/${data.filename}`}
+                alt={data.title}
+              />
+            </div>
             {data.categories &&
               data.categories.map((category) => {
                 return (
-                <div key={category.id}>
+                  <div key={category.id} className="bottomcard">
                     <img
                       src={`http://localhost:4000/Assets/Images/Guide/Icons/${category.icon_filename}`}
                       alt={category.title}
                     />
-                <p>{category.title}</p>
-        
-                </div>)
+                    <h2>{category.title}</h2>
+                    
+                  </div>
+                );
               })}
           </figure>
         </>
